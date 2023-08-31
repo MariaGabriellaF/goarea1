fed21e04de9020ad0dec388fba718428228419da
Maria Gabriella <mgabiisffs@gmail.com>
2023-08-31 13:38:05 -0300

    up

---
+++ goarea1.go
@@ -0,0 +1,17 @@
+package goarea1
+
+import "math"
+
+const Pi = 3.1416
+
+func Circ(raio float64) float64 {
+	return math.Pow(raio, 2) * Pi
+}
+
+func Rect(base, altura float64) float64 {
+	return base * altura
+}
+
+func _TrianguloEqui(base, altura float64) float64 {
+	return (base * altura) / 2
+}
